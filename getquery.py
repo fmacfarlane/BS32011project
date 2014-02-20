a3f71bc6ae3dcf3329ad1432576726fe6b18fc6f
fmacfarlane <f.macfarlane@dundee.ac.uk>
2014-02-20 11:37:18 +0100

    File used to poiint webpage to models.py

--- getquery.py
+++ getquery.py
@@ -3,7 +3,7 @@ import cgi
 import cgitb
 import models
 cgitb.enable()
-
+#imports all of the nessecary modules within python
 form = cgi.FieldStorage()
 print "Content-Type: text/html"
 print
@@ -13,11 +13,11 @@ form=cgi.FieldStorage()
 db=models.DBHandler()
 cursor=db.cursor()
 cursor.execute('select id_ref from probe where gene_id=%s',(form['query'],))
-
+# creates the form to allow the queries to be ran
 print form['query']
 print "<form method=POST action=models.py>"
 print "<table><tr><td>select id_ref from probe where gene_id=</td><td><imput type=text name=query /></td></tr>"
 print "</table>"
 print "</form>"
 print "</body></html>"
-
+# links the form to the models.py file and creates a table for the queries to be submitted and displayed
